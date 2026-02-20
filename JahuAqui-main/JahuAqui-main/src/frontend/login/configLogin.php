48a2969639cec5a40f0ad962b383c2d55939dc08
culas <105617550+lucasszap@users.noreply.github.com>
2026-02-19 21:02:31 -0300

    Delete JahuAqui-main/JahuAqui-main directory

--- JahuAqui-main/JahuAqui-main/src/frontend/login/configLogin.php
+++
@@ -1,7 +0,0 @@
-$host = "localhost";
-$user = "root";
-$pass = "tccjau";
-$banco = "dblogins";
-
-$conexao = @mysqli_connect($host, $user, $pass, $banco)
- or die ("Problemas com a conexão do Banco de Dados");
